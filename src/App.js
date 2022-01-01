86b8c6c93b438758a991de342b79ada26c4a7c85
andycarl2014 <innocentskz42@gmail.com>
2022-01-01 23:02:54 +0200

    1stchange

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 function App() {
 	return (
-		<div className='App'>
-			<h1>hello0</h1>
+		<div>
+			<nav>
+				<ul id='navigation'>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+					<li>
+						<Link to='/about'>About</Link>
+					</li>
+					<li>
+						<Link to='/contact'>Contact</Link>
+					</li>
+				</ul>
+			</nav>
 		</div>
 	);
 }
