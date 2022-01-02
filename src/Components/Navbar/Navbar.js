6884e8d1d8b49d8baeafbb66bab47c9d43ef04b8
andycarl2014 <innocentskz42@gmail.com>
2022-01-02 02:21:55 +0200

    2stchange

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,7 +6,7 @@ function Navbarr() {
 	return (
 		<Navbar className='navbar navbar-expand-md navbar-dark fixed-top bg-dark'>
 			<Container>
-				<Navbar.Brand href='/bagpl/dashboard'>
+				<Navbar.Brand href='#/dashboard'>
 					<img
 						src={logo1}
 						alt='logo'
@@ -15,8 +15,8 @@ function Navbarr() {
 					></img>
 				</Navbar.Brand>
 				<Nav className='me-auto'>
-					<Nav.Link href='/#/dashboard'>Home</Nav.Link>
-					<Nav.Link href='/#/profile'>Profile</Nav.Link>
+					<Nav.Link href='#/dashboard'>Home</Nav.Link>
+					<Nav.Link href='#/profile'>Profile</Nav.Link>
 					<Nav.Link href='#' onClick={logout}>
 						Logout
 					</Nav.Link>
