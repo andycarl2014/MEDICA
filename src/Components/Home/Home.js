e3dbe5e6ffd1a3522fbeb963f2ffc5603cf5a5e4
andycarl2014 <innocentskz42@gmail.com>
2022-01-02 06:20:33 +0200

    2stchange

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
-
+import eu from '../../images/eu.png';
 class Home extends React.Component {
 	render() {
 		return (
-			<div style={{ height: '250px', color: 'red', margin: '150px' }}>
-				Welcome to MEDICA
+			<div className='mijloc'>
+				<h1>
+					"Sănătatea e ca banii, niciodată nu vom avea o adevărată idee de
+					valoarea sa până când o vom pierde."
+					<h4>
+						Constantin Dragos
+						<br />
+						<h7>PhD in Epidemiology and Biomedical Data Science</h7>
+					</h4>
+				</h1>
+				<img src={eu} />
 			</div>
 		);
 	}
