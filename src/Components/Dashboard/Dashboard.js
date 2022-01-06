c927b470c1423069621f6ee3faa436a5c708f5ef
andycarl2014 <innocentskz42@gmail.com>
2022-01-06 06:29:12 +0200

    2stchange

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -2,27 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router';
 import { auth, db } from '../extras/db/firebase';
-import fb from '../extras/images/fb.png';
+
 import FetchDataFromRSSFeed from '../News/News';
 
 function Dashboard() {
-	const [name, setName] = useState('');
-	const [phone, setPhone] = useState('');
-	const [country, setCountry] = useState('');
-	const [sex, setSex] = useState('');
-	const [CNP, setCNP] = useState('');
-	const [age, setAge] = useState('');
-	const [degree, setDegree] = useState('');
-	const [email, setEmail] = useState('');
-
 	const navigate = useNavigate();
-	const fetchUserDetails = newFunction();
 
 	const [user, loading] = useAuthState(auth);
 	useEffect(() => {
 		if (loading) return;
 		if (!user) return navigate('/home', { replace: true });
-		fetchUserDetails();
 	}, [user, loading]);
 	return (
 		<div className='dashUnderNav' style={{ boxShadow: 'none' }}>
@@ -38,15 +27,6 @@ function Dashboard() {
 					.where('uid', '==', user.uid)
 					.get();
 				const data = query.docs[0].data();
-
-				setName(data.name);
-				setEmail(data.email);
-				setCNP(data.CNP);
-				setAge(data.age);
-				setSex(data.sex);
-				setDegree(data.degree);
-				setCountry(data.country);
-				setPhone(data.phone);
 			} catch (err) {
 				console.error(err);
 				alert('An error occured while fetching user data');
